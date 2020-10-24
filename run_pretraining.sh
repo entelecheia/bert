a093ed8279e39191245fe8d323f34f0f0ebaafbc
Youngjoon Lee <yj.lee@yonsei.ac.kr>
2020-10-24 11:24:37 -0700

    mod configs

--- run_pretraining.sh
+++ run_pretraining.sh
@@ -4,14 +4,14 @@ python3 run_pretraining.py \
     --do_train=True \
     --do_eval=True \
     --bert_config_file=config/base_config.json \
-    --train_batch_size=128 \
+    --train_batch_size=64 \
     --max_seq_length=512 \
     --max_predictions_per_seq=20 \
     --num_train_steps=2000000 \
     --num_warmup_steps=10000 \
     --save_checkpoints_steps=25000 \
     --keep_checkpoint_max=30 \
-    --learning_rate=2e-5 \
+    --learning_rate=5e-5 \
     --use_tpu=True \
     --num_tpu_cores=8 \
     --tpu_zone=europe-west4-a \
