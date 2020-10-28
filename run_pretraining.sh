e261d507cc8309a9bfcecadae3f37090c5b4bef3
Youngjoon Lee <yj.lee@yonsei.ac.kr>
2020-10-28 06:26:00 -0700

    mod configs

--- run_pretraining.sh
+++ run_pretraining.sh
@@ -7,7 +7,7 @@ python3 run_pretraining.py \
     --train_batch_size=64 \
     --max_seq_length=512 \
     --max_predictions_per_seq=20 \
-    --num_train_steps=2000000 \
+    --num_train_steps=3000000 \
     --num_warmup_steps=10000 \
     --save_checkpoints_steps=25000 \
     --keep_checkpoint_max=30 \
